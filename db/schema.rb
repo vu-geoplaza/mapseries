ddd017b79441a14ad5a84e8ea4cf0ae7bd50e522
Peter Vos <pjmvos@gmail.com>
2019-08-14 16:41:59 +0200

    final changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190211161146) do
+ActiveRecord::Schema.define(version: 20190808153551) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -120,6 +120,7 @@ ActiveRecord::Schema.define(version: 20190211161146) do
     t.geometry "geom",       limit: {:srid=>28992, :type=>"st_polygon"}
     t.datetime "created_at",                                             null: false
     t.datetime "updated_at",                                             null: false
+    t.geometry "geom4326", limit: {:srid => 4326, :type => "st_polygon"}
   end
 
   create_table "repositories", force: :cascade do |t|
