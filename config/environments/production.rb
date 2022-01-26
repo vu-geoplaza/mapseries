bacbc1b5bd1471b810b9dbe4951df49c2cc7cce5
peer35 <p.j.m.vos@vu.nl>
2022-01-26 13:49:49 +0100

    dockerize

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,8 +26,8 @@ Rails.application.configure do
   # Default Mailer Host
   #Rails.application.routes.default_url_options[:host] = 'pvsge081.labs.vu.nl'
   # prevent host header injection
-  config.action_controller.default_url_options = {host: "discovery.labs.vu.nl"}
-  config.action_mailer.default_url_options = {host: "discovery.labs.vu.nl"}
+  config.action_controller.default_url_options = {host: ENV["SITE_NAME"]}
+  config.action_mailer.default_url_options = {host: ENV["SITE_NAME"]}
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
---
+++ lib/docker-entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# https://stackoverflow.com/a/38732187/1935918
+set -e
+
+if [ -f /usr/src/app/tmp/pids/server.pid ]; then
+  rm /usr/src/app/tmp/pids/server.pid
+fi
+
+#bundle exec rake db:migrate
+
+# Then exec the container's main process (what's set as CMD in the docker-compose.yml).
+exec bundle exec "$@"
